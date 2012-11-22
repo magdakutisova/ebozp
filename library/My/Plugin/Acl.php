28f055f5cb001e0b9be0ed646bc97a8f5eae3007
Bc. Petr Jindra <el.mordo@gmail.com>
2012-11-22 15:46:32 +0100

    Do pluginu ACL byla pridana podpora pro resource modulu

--- library/My/Plugin/Acl.php
+++ library/My/Plugin/Acl.php
@@ -18,6 +18,15 @@ class My_Plugin_Acl extends Zend_Controller_Plugin_Abstract{
 		$resource = $request->getControllerName();
 		$action = $request->getActionName();
 		
+		// zahrnuti modulu do ACL
+		$module = $request->getModuleName();
+		$defModule = Zend_Controller_Front::getInstance()->getDefaultModule();
+		
+		// vyhodnoceni modulu
+		if (!(empty($module) || $module == $defModule)) {
+			$resource = $module . ":" . $resource;
+		}
+		
 		if(!$this->_acl->isAllowed($role, $resource, $action)){
 			if ($role == My_Role::ROLE_GUEST){
 				$request->setControllerName('user')->setActionName('login')->setModuleName("default");
