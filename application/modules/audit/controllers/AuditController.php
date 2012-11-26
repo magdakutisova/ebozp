1fab79384bb4a734816ceb6cce2b60a206e0ebda
Bc. Petr Jindra <el.mordo@gmail.com>
2012-11-27 00:56:25 +0100

    Vytvoreni prostredi pro zalození auditu a jeho zpracovani technikem

--- application/modules/audit/controllers/AuditController.php
+++ application/modules/audit/controllers/AuditController.php
@@ -18,6 +18,64 @@ class Audit_AuditController extends Zend_Controller_Action {
 		$this->_user = $tableUsers->getByUsername($username);
 	}
 	
+	public function createAction() {
+		// nacteni dat a naplneni formulare
+		$data = $this->getRequest()->getParam("audit", array());
+		$data = array_merge(array("subsidiary_id" => 0), $data);
+		
+		// nacteni dat
+		$tableSubsidiaries = new Application_Model_DbTable_Subsidiary();
+		$diary = $tableSubsidiaries->find($data["subsidiary_id"])->current();
+		
+		// pokud nebylo nic nalezeno, vraci se na index
+		if (!$diary) throw new Zend_Exception("Pobočka nenalezena");
+		
+		// kontrola uzivatele
+		$tableAssocs = new Application_Model_DbTable_UserHasSubsidiary();
+		$assoc = $tableAssocs->fetchRow(array(
+				"id_user = " . $this->_user->getIdUser(),
+				"id_subsidiary = " . $diary->id_subsidiary
+		));
+		
+		// kontrola validity asociace
+		if (!$assoc) throw new Zend_Exception("Pobočka není přístupná");
+		
+		// nastaveni formulare
+		$form = new Audit_Form_Audit();
+		$form->fillSelects();
+		
+		// castecna validace dat, pokud je potreba
+		$form->isValidPartial($data);
+		$form->populate($data);
+		
+		$this->view->form = $form;
+	}
+	
+	public function fillAction() {
+		$data = $this->getRequest()->getParam("audit");
+		$data = array_merge(array("id" => 0), $data);
+		
+		// nacteni auditu
+		$tableAudits = new Audit_Model_Audits();
+		$audit = $tableAudits->getById($data["id"]);
+		
+		if (!$audit) throw new Zend_Exception("Audit #" . $data["id"] . " has not been found");
+		
+		// nacteni dotazniku
+		$tableQuestionaries = new Questionary_Model_Filleds();
+		$questionaryRow = $tableQuestionaries->getById($audit->form_filled_id);
+		
+		$questionary = $questionaryRow->toClass();
+		$form = new Audit_Form_AuditFill();
+		
+		$form->populate($audit->toArray());
+		$form->populate($data)->isValidPartial($data);
+		
+		$this->view->questionary = $questionary;
+		$this->view->audit = $audit;
+		$this->view->form = $form;
+	}
+	
 	public function indexAction() {
 		// nacteni klientu a pobocek, ktere jsou k dispozici
 		$subSiDiariesIds = $this->_user->getUserSubsidiaries();
@@ -53,5 +111,77 @@ class Audit_AuditController extends Zend_Controller_Action {
 		// zapis do view
 		$this->view->clients = $clients;
 		$this->view->subSiDiaryIndex = $subSiDiaryIndex;
+	}
+	
+	public function postAction() {
+		// nacteni dat
+		$data = $this->getRequest()->getParam("audit");
+		
+		// nacteni a validace formulare
+		$form = new Audit_Form_Audit();
+		$form->fillSelects();
+		
+		if (!$form->isValid($data)) {
+			$this->_forward("create");
+			return;
+		}
+		
+		// nacteni dat
+		$tableUser = new Application_Model_DbTable_User();
+		$tableSubsidiaries = new Application_Model_DbTable_Subsidiary();
+		$tableAudits = new Audit_Model_Audits();
+		
+		$subsidiary = $tableSubsidiaries->find($form->getValue("subsidiary_id"))->current();
+		$coordinator = $tableUser->find($form->getValue("coordinator_id"))->current();
+		$auditor = $tableUser->find($this->_user->getIdUser())->current();
+		
+		// datum provedeni
+		$doneAt = new Zend_Date($form->getValue("done_at"), "MM. dd. y");
+		
+		// nacteni radku fomrulare
+		$tableForms = new Audit_Model_Forms();
+		$formRow = $tableForms->find($form->getValue("form_id"))->current();
+		
+		// vytvoreni zaznamu
+		$audit = $tableAudits->createAudit($auditor, $coordinator, $formRow, $subsidiary, $doneAt, array());
+		
+		$this->_redirect("/audit/audit/fill?audit[id]=" . $audit->id);
+	}
+	
+	public function putAction() {
+		// nacteni dat
+		$data = $this->getRequest()->getParam("audit");
+		
+		$form = new Audit_Form_AuditFill();
+		$form->getElement("summary")->setRequired(false);
+		
+		// kontrola validity
+		$form->populate($data);
+		if (!$form->isValidPartial($data)) $this->_forward("fill");
+		
+		// nacteni auditu
+		$tableAudits = new Audit_Model_Audits();
+		$audit = $tableAudits->getById($form->getValue("id"));
+		
+		// nacteni formulare
+		$filled = $audit->findParentRow("Questionary_Model_Filleds");
+		$questionary = $filled->toClass();
+		
+		// ulozeni formulare
+		$questionary->setFromArray(Zend_Json::decode($data["content"]));
+		$filled->saveFilledData($questionary);
+		
+		// zapis poznamek a shrnuti
+		$audit->summary = $form->getValue("summary");
+		$audit->progress_note = $form->getValue("progress_note");
+		
+		$audit->save();
+		
+		// presmerovani na fill
+		$this->_redirect("/audit/audit/fill?audit[id]=" . $audit->id);
+	}
+	
+	protected function submit(Audit_Form_AuditFill $form, Audit_Model_Row_Audit $audit) {
+		
 	}
 }
---
+++ application/modules/audit/forms/Audit.php
@@ -0,0 +1,104 @@
+<?php
+class Audit_Form_Audit extends Zend_Form {
+	
+	public function fillSelects() {
+		// nalezeni formularu auditu
+		$tableForms = new Audit_Model_Forms();
+		$forms = $tableForms->fetchAll(null, "name");
+		
+		// zapis formularu
+		foreach ($forms as $item) {
+			$this->getElement("form_id")->addMultiOption($item->questionary_id, $item->name);
+		}
+		
+		// nactei koordinatoru
+		$tableUsers = new Application_Model_DbTable_User();
+		$coordinators = $tableUsers->fetchAll("role = " . My_Role::ROLE_COORDINATOR, "username");
+		
+		// zapis do formulare
+		foreach ($coordinators as $coord) {
+			$this->getElement("coordinator_id")->addMultiOption($coord->id_user, $coord->username);
+		}
+	}
+	
+	public function init() {
+		// nastaveni dat
+		$this->setName("audit-edit");
+		$this->setMethod(Zend_Form::METHOD_POST);
+		$this->setElementsBelongTo("audit");
+		$this->setAction("/audit/audit/post");
+		
+		// nastaveni dekoratoru
+		$this->setDecorators(array(
+				'FormElements',
+				array('HtmlTag', array('tag' => 'table')),
+				'Form',
+		));
+		
+		$elementDecorator = array(
+				'ViewHelper',
+				array('Errors'),
+				array(array('data' => 'HtmlTag'), array('tag' => 'td', 'class' => 'element')),
+				array('Label', array('tag' => 'td')),
+				array(array('row' => 'HtmlTag'), array('tag' => 'tr')),
+		);
+		
+		$lastDecoratorOpen = array(
+				"ViewHelper",
+				array(array("data" => "HtmlTag"), array("tag" => "td", "class" => "element", "colspan" => 2, "openOnly" => true)),
+				array(array("row" => "HtmlTag"), array("tag" => "tr", "openOnly" => true))
+		);
+		
+		$lastDecoratorClose = array(
+				"ViewHelper",
+				array(array("data" => "HtmlTag"), array("tag" => "td", "class" => "element", "colspan" => 2, "closeOnly" => true)),
+				array(array("row" => "HtmlTag"), array("tag" => "tr", "closeOnly" => true))
+		);
+		
+		// zodpovedne osoby
+		$this->addElement("text", "responsibile_name", array(
+				"required" => true,
+				"decorators" => $elementDecorator,
+				"label" => "Zodpovědná osoba*"
+		));
+		
+		// datum provedeni auditu
+		$this->addElement("text", "done_at", array(
+				"decorators" => $elementDecorator,
+				"label" => "Datum provedení auditu",
+				"required" => true,
+				"validators" => array(
+						array(
+								"Regex",
+								false,
+								array(
+										"pattern" => "/^([0-9]{2}\. ){2}[0-9]{4}$/",
+										"messages" => "Špatný formát datumu"
+								)
+						)
+				)
+		));
+		
+		// typ formulare
+		$this->addElement("select", "form_id", array(
+				"decorators" => $elementDecorator,
+				"label" => "Formulář auditu"
+		));
+		
+		// koordinator
+		$this->addElement("select", "coordinator_id", array(
+				"decorators" => $elementDecorator,
+				"label" => "Koordinátor auditu"
+		));
+		
+		// skryta pole, tlacitka a tak
+		$this->addElement("hidden", "subsidiary_id", array(
+				"decorators" => $lastDecoratorOpen
+		));
+		
+		$this->addElement("submit", "submit", array(
+				"decorators" => $lastDecoratorClose,
+				"label" => "Provést audit"
+		));
+	}
+}
