6834d79779c297eb5c51bab2f4888a3aff4525f3
Bc. Petr Jindra <el.mordo@gmail.com>
2012-12-29 17:26:19 +0100

    Ještě jednou oprava helperu SqlDate - překlep

--- application/modules/audit/views/helpers/SqlDate.php
+++ application/modules/audit/views/helpers/SqlDate.php
@@ -4,7 +4,7 @@ class Zend_View_Helper_SqlDate extends Zend_View_Helper_Abstract {
 	public function sqlDate($date) {
 		$parts = explode(" ", $date);
 		
-		list ($year, $month, $day) = explode("-", $date[0]);
+		list ($year, $month, $day) = explode("-", $parts[0]);
 		
 		return "$day. $month. $year";
 	}
