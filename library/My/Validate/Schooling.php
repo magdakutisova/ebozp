d28d62044ee1e530cfd243c786ddbb6de6144f2c
magdakutisova <kutisovamagda@gmail.com>
2013-01-17 20:59:16 +0100

    dokonceni vkladani skoleni neuvedenych v seznamu, validace skoleni a debugging
    Change-Id: I90396d9bed24ad24748019b53d9a95a5411cfbd9

--- library/My/Validate/Schooling.php
+++ library/My/Validate/Schooling.php
@@ -2,13 +2,19 @@
 class My_Validate_Schooling extends Zend_Validate_Abstract{
 	
 	const INVALID_DATE = 'invalidDate';
+	const NO_NAME = 'noName';
 	protected $_messageTemplates = array(
 			self::INVALID_DATE => 'Zadejte datum ve formátu DD.MM.RRRR',
+			self::NO_NAME => 'Zadejte název školení',
 			);
 	
 	public function isValid($value){
+		if(($value['last_execution'] != '' || $value['note'] != '' || $value['private'] != '') && ($value['schooling'] == '' || $value['schooling'] == 0)){
+			$this->_error(self::NO_NAME);
+			return false;
+		}
 		$dateValidator = new Zend_Validate_Date(array('format' => 'dd.MM.yyyy'));
-		if(!$dateValidator->isValid($value['last_execution'])){
+		if(($value['schooling'] != '' || $value['schooling'] != 0) && $value['last_execution'] != '' && !$dateValidator->isValid($value['last_execution'])){
 			$this->_error(self::INVALID_DATE);
 			return false;
 		}
