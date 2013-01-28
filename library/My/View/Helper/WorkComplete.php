cf6f626fbef874f295fbac1147f0049e3daa15e4
magdakutisova <kutisovamagda@gmail.com>
2013-01-28 23:03:49 +0100

    úprava zobrazování výběru pracovišť
    Change-Id: I207b0bb7325f9c1638ad052d5d033541599938c5

--- library/My/View/Helper/WorkComplete.php
+++ library/My/View/Helper/WorkComplete.php
@@ -10,7 +10,7 @@ class My_View_Helper_WorkComplete extends Zend_View_Helper_FormElement{
 		$multiOptions = isset($attribs['multiOptions']) ? $attribs['multiOptions'] : null;
 		$multiOptions2 = isset($attribs['multiOptions2']) ? $attribs['multiOptions2'] : null;
 		$multiOptions3 = isset($attribs['multiOptions3']) ? $attribs['multiOptions3'] : null;
-		
+			
 		if($value){
 			$idWork = $value['id_work'];
 			$work = $value['work'];
@@ -40,7 +40,12 @@ class My_View_Helper_WorkComplete extends Zend_View_Helper_FormElement{
 		$this->html .= $helperText->formText($name . '[new_work]', $newWork) . '</td>';
 		$this->html .= '</tr><tr>';
 		$this->html .= '<td colspan="3"><label for="' . $name . '[workplaces]">Vyberte pracoviště, kde je pracovní činnost vykonávána</label><br/>';
-		$this->html .= $helperMultiCheckbox->formMultiCheckbox($name . '[workplaces]', $workplaces, null, $multiOptions2) . '</td>';
+		if($multiOptions2 != 0){
+			$this->html .= '<div class="multiCheckbox">' . $helperMultiCheckbox->formMultiCheckbox($name . '[workplaces]', $workplaces, null, $multiOptions2) . '</div></td>';
+		}
+		else{
+			$this->html .= '<div class="multiCheckbox">Tento klient nemá zatím žádná pracoviště.</div>';
+		}
 		$this->html .= '<td colspan="3"><label for="' . $name . '[new_workplaces]">Případně zadejte v seznamu neuvedená pracoviště (každé na nový řádek)</label><br/>';
 		$this->html .= $helperTextarea->formTextarea($name . '[new_workplaces]', $newWorkplaces) . '</td>';
 		$this->html .= '</tr><tr>';
