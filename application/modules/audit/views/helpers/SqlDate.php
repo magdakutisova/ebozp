8e1551cff0ff3adefdba01da642b90e129ce5713
Bc. Petr Jindra <el.mordo@gmail.com>
2013-11-09 20:44:56 +0100

    Pridana prima editace neshody z registru opraven pripad prazdneho data v helperu

--- application/modules/audit/views/helpers/SqlDate.php
+++ application/modules/audit/views/helpers/SqlDate.php
@@ -2,6 +2,8 @@
 class Zend_View_Helper_SqlDate extends Zend_View_Helper_Abstract {
 	
 	public function sqlDate($date) {
+		if (!$date) return "-";
+		
 		$parts = explode(" ", $date);
 		
 		list ($year, $month, $day) = explode("-", $parts[0]);
